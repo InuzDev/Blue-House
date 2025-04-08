3df76d1fed9b33067b980508ef73717c594fccb5
InuDeveloper <shiibadev@gmail.com>
2025-04-08 07:06:00 -0400

    Working on the API call and connection, fixing bugs and deploying the API

--- IoT/include/secret_example.h
+++ IoT/include/secret_example.h
@@ -1,9 +1,9 @@
 #ifndef SECRETS_H
 #define SECRETS_H
 
-#define SERVER_ADDRESS "" // Server address here
-#define BACKEND_PORT 8000 // Backend port here
-#define WIFI_SSID ""      // Network name here
-#define WIFI_PASSWORD ""  // Network password here
+#define SERVER_ADDRESS "0.0.0.0"         // Server address here
+#define BACKEND_PORT 8000                // Backend port here
+#define WIFI_SSID "NETWORK_NAME"         // Network name here
+#define WIFI_PASSWORD "NETWORK_PASSWORD" // Network password here
 
 #endif
--- IoT/src/main.cpp
+++ IoT/src/main.cpp
@@ -29,13 +29,13 @@ void setup()
    // Else, if there is a shield, attempt to connect to Wi-Fi network
    while (status != WL_CONNECTED)
    {
-      Serial.print("Attempting to connect to the Network/SSID:");
+      Serial.print("Attempting to connect to the Network/SSID: ");
       Serial.println(WIFI_SSID);
 
       // Connect to the desired network
       status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-      delay(5000); // Wait 5 seconds before retrying
+      delay(2000); // Wait 2 seconds before retrying
    }
 
    Serial.println("Connected to WiFi!"); // Print this if the connectio is succesful
@@ -52,9 +52,24 @@ void loop()
    int statusCode = client.responseStatusCode();
    String response = client.responseBody();
 
+   // Check to the Server address
+   Serial.print("\nTrying to connect to: ");
+   Serial.println(SERVER_ADDRESS);
+
    // Print the status code of the response
    Serial.print("Status code: ");
-   Serial.println(statusCode);
+
+   // Check if there was an error and print a readable message.
+   if (statusCode == -2)
+   {
+      Serial.print("Connection failed, error code: ");
+      Serial.print(statusCode);
+      Serial.println(", Retrying in 12 seconds\n");
+   }
+   else
+   {
+      Serial.println(statusCode);
+   }
 
    // Print the response
    Serial.print("Response: ");
